f4ebf84380e8b2f2193d80979adc772172f90671
cheriseba <cherise.barthel@stud-hawk.de>
2022-06-26 14:52:17 +0200

    gameone
    arrange tiles

--- gameone/gameone.js
+++ gameone/gameone.js
@@ -1,73 +1,103 @@
-let tiles = [];
-let bubbles =[];
+img = []
 
-function preload () {
 
-    for (let i = 1; i < 16; i++) {
-tiles[i]= loadImage ("tiles/tile"+i+".png")
-    }
+function preload() {
+
+for ( let i = 0; i < 16; i++) {
+
+img[i] =loadImage(`tiles/tile${i}.png`)
+
+}
+
 }
 
 function setup() {
+    createCanvas(1024, 1024);
+    
+  }
+  
+  function draw() {
 
-    createCanvas(1000,1000);
+let ya = 64
 
-    for (let i = 0; i< 10; i++) {
-        let x = random(width);
-        let y = random(height);
-        let r = random(20,60);
-        let tile = random(tiles);
-        let b = new Bubble(x,y,r,pic);
-        bubbles.push(b);
-    }
+
+background(220);
+noFill();
+
+
+for (let x = -0.5; x < 16; x++) {rect(x*64, -48 , 64, 64)}
+
+        for (let x = 0; x < 16; x++) {rect(x*64,0, 64, 64);}
+
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16 , 64, 64)}
+
+        for (let x = 0; x < 16; x++) {rect(x*64,ya,64, 64);}
+
+for (let x = -0.5; x < 16; x++) {image(random(img),x*64, 16+ya , 64, 64)}
+  
+        for (let x = 0; x < 16; x++) {image(random(img),x*64, ya*2,64, 64);}
+
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*2 , 64, 64)}
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*3,64,64);}
+          
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*3 , 64, 64)} 
+
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*4,64,64);}
+           
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*4 , 64, 64)}        
+
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*5,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*5 , 64, 64)}    
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*6,64,64);}
+
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*6 , 64, 64)}               
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*7,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*7 , 64, 64)} 
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*8,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*8 , 64, 64)}       
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*9,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*9 , 64, 64)}   
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*10,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*10 , 64, 64)}   
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*11,64,64);} 
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*11 , 64, 64)}    
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*12,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*12 , 64, 64)}     
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*13,64,64);}
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*13 , 64, 64)}      
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*14,64,64);}        
+                
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*14 , 64, 64)}        
+                
+        for (let x = 0; x < 16; x++) {rect(x*64,ya*15,64,64);}      
+
+for (let x = -0.5; x < 16; x++) {rect(x*64, 16+ya*15 , 64, 64)}
+
+
+
+noLoop();
+
+    // for (let x = -0.5; x < 16; x++) {
+    //     for (let y = -0.75; y < 16; y++){
+    //     rect(x*64, y*64, 64, 64);
+    //     }
+    //   }
 }
-
-function mousePressed() {
-
-for (let i = 0; i< bubbles.length; i++) {
-bubbles[i].cllicked(mouseX,mouseY);
-}
-}
-
-function draw() {
-
-    background(0);
-    for(let i = 0; i < bubbles.length;i++) {
-        bubbles[i].move();
-        bubbles[i].show();
-    }
-}
-
-class Bubble {
-
-    constructor(x,y,r,img) {
-            this.x =x;
-            this.y =y;
-            this.r =r;
-
-            this.tile = img;
-    }
-
-clicked(px,py) {
-
-    let d = dist(px,py, this.x,this,y);
-    if(d<this.r) {
-        this.tile = random(tiles)
-   
-    }
-}
-
-move() {
-this.x = this.x + random(-2,2);
-this.y = this.y + random(-2,2);
-}
-
-show() {
-image(this.tile, this.x, this.y,this.r,this.r)
-
-
-}
-}
-
-
-
